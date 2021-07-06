e267a3b0b2ecd4be84d1a8701d048ac2361615c5
Anusha Marella <48984998+anushamarella9@users.noreply.github.com>
2021-07-06 11:07:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,136 +7,73 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
 
 import pymongo
 from pymongo import MongoClient
+from datetime import datetime
+from bson import ObjectId
 
 client = MongoClient()
-client = MongoClient('localhost',27017) 
+client = MongoClient('localhost',27017)
 
-cors = CORS(app, resources={r"/add_user": {"origins": "http://localhost:5000"}})
+#cors = CORS(app, resources={r"/add_user": {"origins": "http://localhost:5000"}})
 cors = CORS(app, resources={r"/add_task": {"origins": "http://localhost:5000"}})
-cors = CORS(app, resources={r"/update_user": {"origins": "http://localhost:5000"}})
-cors = CORS(app, resources={r"/delete_user": {"origins": "http://localhost:5000"}})
-cors = CORS(app, resources={r"/get_user": {"origins": "http://localhost:5000"}})
-cors = CORS(app, resources={r"/get_tasks_for_user/<username>": {"origins": "http://localhost:5000"}})
-cors = CORS(app, resources={r"/update_task": {"origins": "http://localhost:5000"}})
-cors = CORS(app, resources={r"/delete_task": {"origins": "http://localhost:5000"}})
+cors = CORS(app, resources={r"/get_all_tasks": {"origins": "http://localhost:5000"}})
+cors = CORS(app, resources={r"/update_task/<task_id>": {"origins": "http://localhost:5000"}})
+cors = CORS(app, resources={r"/delete_task/<id>": {"origins": "http://localhost:5000"}})
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/add_user/", methods=["POST"])
-@cross_origin(origin='localhost',headers=['Content-Type'])
-def addUser():
-    db = client['user_info']
-    users = db.user_info
-    name = request.json.get('name')
-    user_id = request.json.get('id')
-
-    new_list = {'name':name,'id':user_id}
-    db_query = users.insert_one(new_list)
-
-    result = {'result' : 'user is added successfully','status_code':1}
-
-    return result
-
-@app.route("/add_task/", methods=["POST"])
+@app.route("/add_task", methods=["POST"])
 @cross_origin(origin='localhost',headers=['Content-Type'])
 def addTask():
-    db = client['user_info']
-    users = db.user_info
-
-    name = request.json.get('name')
+    db = client['task_info']
+    tasks = db.task_info
 
     task = request.json.get('task')
+    id = request.json.get('id')
+    added_on = str(datetime.now())
 
-    #new_list = {'name':name,'task':task}
-    #db_query = users.insert_one(new_list)
+    new_list = {'task':task,'added_on':added_on,'id':id}
+    db_query = tasks.insert_one(new_list)
 
-    users.update_one({'name':name},{'$push':{'task':task}})
-    result = {'result' : 'task is added successfully','status_code':1}
+    result = {'message' : 'task is added successfully','status_code':1}
 
     return result
 
 
-@app.route("/get_user/", methods=["GET"])
+@app.route("/get_all_tasks", methods=["GET"])
 @cross_origin(origin='localhost',headers=['Content-Type'])
-def getUser():
-    
-    db = client['user_info']
-    users = db.user_info
+def getTasks():
+    db = client['task_info']
+    tasks = db.task_info
     result = []
-    for user in users.find():
-        result.append({'name' : user['name'],'id':user['id'],'task':user['task']})
-        
-    return jsonify({'results' : result,'status_code':1})
-    
-@app.route("/update_user/", methods=["PUT"])
-@cross_origin(origin='localhost',headers=['Content-Type'])
-def updateUser():
-    db = client['user_info']
-    users = db.user_info
-
-    name = request.json.get('name')
-    user_id = request.json.get('id')
-
-    updated_list = {"$set": {'id' : user_id}}
-    edit_info = {'name' : name}
-    users.update_one(edit_info, updated_list)
-    result = {'result' : 'Updated successfully','status_code':1}
-    return result
-
-@app.route("/delete_user/", methods=["DELETE"])
-@cross_origin(origin='localhost',headers=['Content-Type'])
-def deleteUser():
-    db = client['user_info']
-    users = db.user_info
-    user_id = request.json.get('id')
-
-    delete_data = {'id' : user_id}
-    users.delete_one(delete_data)
-    result = {'result' : 'Deleted successfully','status_code':1}
-    return result
-
-@app.route("/get_tasks_for_user/<username>/", methods=["GET"])
-@cross_origin(origin='localhost',headers=['Content-Type'])
-def getTasks(username):
-
-    db = client['user_info']
-    users = db.user_info
-    username = username
-
-    result = []
-    for task in users.find({'username':username}):
-        result.append({'task' : task['task'], 'name':task['name']})
+    for task in tasks.find():
+        print("!!!!!!!!",task)
+        result.append({'task' : task['task'],'added_on':task[str('added_on')],'id':task['id']})
     return jsonify({'results' : result,'status_code':1})
 
 
-@app.route("/update_task/", methods=["PUT"])
+@app.route("/update_task/<task_id>", methods=["PUT"])
 @cross_origin(origin='localhost',headers=['Content-Type'])
-def updateTask():
-    db = client['user_info']
-    users = db.user_info
+def updateTask(task_id):
+    db = client['task_info']
+    tasks = db.task_info
+    task = request.json.get('task')
 
-    name = request.json.get('name')
-    task_id = request.json.get('id')
+    tasks.update_one({"id":task_id}, {"$set": {"task": task}})
 
-    updated_list = {"$set": {'id' : task_id}}
-    edit_info = {'name' : name}
-    users.update_one(edit_info, updated_list)
     result = {'result' : 'Updated successfully','status_code':1}
     return result
 
 
-@app.route('/delete_task/', methods=['DELETE'])
+@app.route('/delete_task/<id>', methods=["DELETE"])
 @cross_origin(origin='localhost',headers=['Content-Type'])
-def deleteTask():
-    db = client['user_info']
-    users = db.user_info
-    user_id = request.json.get('id')
+def deleteTask(id):
+    db = client['task_info']
+    tasks = db.task_info
 
-    delete_data = {'id' : user_id}
-    users.delete_one(delete_data)
+    delete_data = {"id":id}
+    tasks.delete_one(delete_data)
     result = {'result' : 'Deleted successfully','status_code':1}
     return result
 
 if __name__ == '__main__':
     app.run()
-
